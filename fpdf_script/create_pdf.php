65d527b19aea6c33c700e566fe7c1ea3751dda85
mriantf9 <muhamad@riantf.id>
2020-12-10 11:10:41 +0700

    edit fpdf

--- fpdf_script/create_pdf.php
+++ fpdf_script/create_pdf.php
@@ -1,10 +1,10 @@
 <?php 
 
-$DIR="/home/mriantf/script_skripsi"
-$DT_RP= $DIR."/DATA_REPORT/Daily"
-$WORKDIR= $DIR."/script"
-$SCRIPTDIR= $WORKDIR."/fpdf_script"
-$OUTPUT= $DIR."/OUTPUT_PDF"
+$DIR="/home/mriantf/script_skripsi";
+$DT_RP= $DIR."/DATA_REPORT/Daily";
+$WORKDIR= $DIR."/script";
+$SCRIPTDIR= $WORKDIR."/fpdf_script";
+$OUTPUT= $DIR."/OUTPUT_PDF";
 
 echo $OUTPUT
 
