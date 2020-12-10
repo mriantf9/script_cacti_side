446dbde39fec6e67ace134950044a5b6534bff9f
mriantf9 <muhamad@riantf.id>
2020-12-10 11:08:30 +0700

    create php pdf

---
+++ fpdf_script/create_pdf.php
@@ -0,0 +1,16 @@
+<?php 
+
+$DIR="/home/mriantf/script_skripsi"
+$DT_RP= $DIR."/DATA_REPORT/Daily"
+$WORKDIR= $DIR."/script"
+$SCRIPTDIR= $WORKDIR."/fpdf_script"
+$OUTPUT= $DIR."/OUTPUT_PDF"
+
+echo $OUTPUT
+
+
+
+
+
+
+?>
