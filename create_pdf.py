6f7c07384ec426ebfb3093d344a08151e6e487be
mriantf9 <muhamad@riantf.id>
2020-12-10 14:59:43 +0700

    test list dir

--- create_pdf.py
+++ create_pdf.py
@@ -3,19 +3,34 @@
 from fpdf import FPDF
 from os import listdir
 import sys
+import csv
 
-###### DEFINE DIRECTORY ######
+##FOR FILTER##
+import glob
+
+##FOR SEPARATE##
+import re
+
+###### DEFINE ######
 DIR = "/home/mriantf/script_skripsi"
+DT_DIR = DIR+"/DATA_REPORT"
 SRC_IMG= DIR+"/OUTPUT"
 OUTPUT_PDF = DIR+"/OUTPUT_PDF"
+GTYPE = sys.argv[1]
+
+LISTIMG = listdir(SRC_IMG)
+
+csv_list = listdir(DT_DIR+'/'+GTYPE)
+
+print(csv_list)
 
 
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
+# pdf = FPDF('P','mm','A4')
+# pdf.add_page()
+# pdf.set_font("Arial", size=12)
 
-### TEXT CENTER ###
-pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+# ### TEXT CENTER ###
+# pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
 
 
 
@@ -29,7 +44,7 @@ pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
 
 
 ### OUTPUT ###
-pdf.output("/mnt/d/LARAVEL/simple_demo.pdf")
+# pdf.output("/mnt/d/LARAVEL/simple_demo.pdf")
 
 
 
