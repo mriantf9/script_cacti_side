4bc23aacb4b918a7097ab614adce5a511fda4d30
mriantf9 <muhamad@riantf.id>
2020-12-11 06:49:34 +0700

    rubah else

--- create_pdf.py
+++ create_pdf.py
@@ -40,13 +40,13 @@ for filecsv in csv_list:
                 RRDTITLE = row[6]
                 PERIODIC = row[7]
                 list_img = [os.system("ls "+SRC_IMG+'/'+GTYPE+"| grep ReportID"+IDREPORT)]
-                else:
-                    break
-                for imglist in list_img:
-                    #subprocess.call(['/bin/grep', imglist])
-                    print(imglist)
-                    #exit()
-                    #pdf.add_page()
-                    #pdf.set_font("Arial", size=12)
-                    #pdf.image(imglist,x=50,y=100,w=20,h=5)
-                #pdf.output(OUTPUT_PDF+GTYPE+'/'+TITLE+".pdf", 'F')
+            else:\
+                break
+            for imglist in list_img:
+                #subprocess.call(['/bin/grep', imglist])
+                print(imglist)
+                #exit()
+                #pdf.add_page()
+                #pdf.set_font("Arial", size=12)
+                #pdf.image(imglist,x=50,y=100,w=20,h=5)
+            :#pdf.output(OUTPUT_PDF+GTYPE+'/'+TITLE+".pdf", 'F')
