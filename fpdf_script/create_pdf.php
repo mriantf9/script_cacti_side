7a9fa2451e8c34a49d3349982153bded757cd41e
mriantf9 <muhamad@riantf.id>
2020-12-10 11:29:16 +0700

    edit create_php2

--- fpdf_script/create_pdf.php
+++ fpdf_script/create_pdf.php
@@ -6,4 +6,17 @@ $WORKDIR = ($DIR . "/script");
 $SCRIPTDIR = getCwd();
 $OUTPUT = ($DIR . "/OUTPUT_PDF");
 
-echo "$SCRIPTDIR, $OUTPUT";
+require('$SCRIPTDIR/fpdf.php');
+
+// intance object dan memberikan pengaturan halaman PDF
+$pdf = new FPDF('P', 'mm', 'A4');
+
+// membuat halaman baru
+$pdf->AddPage();
+
+// setting jenis font yang akan digunakan
+$pdf->SetFont('Arial', 'B', 16);
+
+// mencetak string 
+$pdf->Cell(0, 15, 'Graphic Pemakaian', '0', 1, 'C');
+$pdf->Output();
