6f6ff33bca77f78f685ff968577cde09d9eba216
mriantf9 <muhamad@riantf.id>
2020-12-12 01:13:43 +0700

    bismillah, bisaa

--- create_pdf.py
+++ create_pdf.py
@@ -57,9 +57,9 @@ for filecsv in csv_list:
             pdf.cell(190, 10, txt=TITLE, ln=1, align="C")
             idx = 1
             for imglist in filelist:
-                pdf.cell(0, 10, str(idx) + '. Traffic Pemakaian ' + RRDTITLE, 0, 1)
+                # pdf.cell(0, 10, str(idx) + '. Traffic Pemakaian ' + RRDTITLE, 0, 1)
                 pdf.image(SRC_IMG+'/'+GTYPE+'/'+imglist, 50, 50, 100)
-                idx += 1
+                # idx += 1
             # for i in range(1, count_array):
             #     pdf.cell(0, 10, str(i) + '. Traffic Pemakaian ' + RRDTITLE, 0, 1)
     pdf.output(OUTPUT_PDF+'/'+GTYPE+'/'+"ReportID"+IDREPORT+"_"+TITLE+".pdf")
