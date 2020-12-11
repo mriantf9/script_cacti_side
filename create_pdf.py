37f07af04690f5436c22ebe562858fa2635f4b7b
mriantf9 <muhamad@riantf.id>
2020-12-11 23:43:15 +0700

    cobainnss

--- create_pdf.py
+++ create_pdf.py
@@ -59,7 +59,7 @@ for filecsv in csv_list:
             for i in range(1, count_array):
                 pdf.cell(0, 10, str(i) + '. Traffic Pemakaian ' + RRDTITLE, 0, 1)
 
-pdf.output(TITLE+".pdf")
+        pdf.output("ReportID"+IDREPORT+"_"+TITLE+".pdf")
             
 
 #print (filelist)
