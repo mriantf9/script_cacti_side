e616dc554ff99032f8c9c341daca5acb88857697
mriantf9 <muhamad@riantf.id>
2020-12-11 09:05:28 +0700

    Bismillah, bisa joss

--- create_pdf.py
+++ create_pdf.py
@@ -47,12 +47,12 @@ for filecsv in csv_list:
                 PERIODIC = row[7]
             else:
                 break
-list_img = [os.system("ls "+SRC_IMG+'/'+GTYPE+"| grep ReportID"+IDREPORT)]
-for imglist in list_img:
+#list_img = [os.system("ls "+SRC_IMG+'/'+GTYPE+"| grep ReportID"+IDREPORT)]
+for imglist in LISTIMG:
     #subprocess.call(['/bin/grep', imglist])
     print(imglist)
     #exit()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    pdf.image(SRC_IMG+'/'+GTYPE+imglist, 0, 0)
+    pdf.image(SRC_IMG+'/'+GTYPE+imglist, w=500, h=120)
     pdf.output(OUTPUT_PDF+GTYPE+'/'+TITLE+".pdf",)
