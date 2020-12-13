5468bd2728e3f8329fff03bb76e2884b210f75e6
mriantf9 <muhamad@riantf.id>
2020-12-14 00:41:56 +0700

    add date

--- create_pdf.py
+++ create_pdf.py
@@ -49,23 +49,36 @@ for filecsv in csv_list:
             IDREPORT = row[0]
             EMAIL = row[2]
             TITLE = row[3]
+            PDFNAME = TITLE.replace(" ", "_")
             RRDTITLE = row[6]
             RRDTITLE2 = RRDTITLE.replace(" ", "_")
             RRDTITLE3 = RRDTITLE2.replace("/","-")
             PERIODIC = row[7]
             filelist = fnmatch.filter(os.listdir(SRC_IMG+'/'+GTYPE), "*"+RRDTITLE3+"*")
+
             
             count_array = len(filelist) + 1
             idx = 1
             for imglist in filelist:
                 path = SRC_IMG+'/'+GTYPE+'/'+imglist
                 pdf.add_page()
-                pdf.set_font("Arial", size=15)
-                pdf.cell(250, 20, txt=TITLE, ln=6, align="C")
+                pdf.set_font("Times", size=15)
+                pdf.cell(250, 20, txt=TITLE, ln=1, align="C")
+                if GTYPE == "Daily":
+                    ystrd = os.system("date -d now-1days +%d %B %Y")
+                    pdf.cell(250, 20, txt=ystrd, ln=6, align="C")
+                elif GTYPE == "Weekly":
+                    lstweek = os.system("date -d now-7days +%d %B %Y")
+                    tdy = os.system("date -d now +%d %B %Y")
+                    pdf.cell(250, 20, txt="From "+lstweek+" - "+tdy, ln=6, align="C")
+                elif GTYPE == "Monthly":
+                    pdf.cell(250, 20, txt=LAST_MONTH, ln=6, align="C")
+                else:
+                    break
                 pdf.cell(0, 30, str(idx) + '. Traffic Pemakaian ' + RRDTITLE, 0, 1)
                 pdf.ln(10)
                 pdf.image(SRC_IMG+'/'+GTYPE+'/'+imglist, 45, 60, 190, 80)
                 idx += 1
-    pdf.output(OUTPUT_PDF+'/'+GTYPE+'/'+"ReportID"+IDREPORT+"_"+TITLE+".pdf")
+    pdf.output(OUTPUT_PDF+'/'+GTYPE+'/'+"ReportID"+IDREPORT+"_"+PDFNAME+".pdf")
             
 
