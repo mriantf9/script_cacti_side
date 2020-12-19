a04d63de763a6fbc40d5940fad36f8d1e42dce27
mriantf9 <muhamad@riantf.id>
2020-12-20 01:00:31 +0700

    ..

--- create_pdf.py
+++ create_pdf.py
@@ -129,9 +129,9 @@ for filecsv in csv_list:
            os.system("/usr/bin/bash "+DIR+'/script/running_mail.sh ' +EMAIL+" "+PDFFILELIST+" "+GTYPE)
            os.system("cp "+OUTPUT_PDF+'/'+GTYPE+'/'+PDFFILELIST+ " "+ARCHV+'/'+GTYPE+'/')
 
-    # try:
-    #      os.system("rm -rf "+DT_DIR+'/'+GTYPE+'/'+"ReportID_"+IDREPORT+"*")
-    #      os.system("rm -rf "+SRC_IMG+'/'+GTYPE+'/'+REPORT+"*")
-    # except OSError as e:
-    #      print("Error: %s : %s" % (SRC_IMG+'/'+GTYPE, e.strerror))
-    #      print("Error: %s : %s" % (DT_DIR+'/'+GTYPE, e.strerror))
+    try:
+         os.system("rm -rf "+DT_DIR+'/'+GTYPE+'/'+"ReportID_"+IDREPORT+"*")
+         os.system("rm -rf "+SRC_IMG+'/'+GTYPE+'/'+REPORT+"*")
+    except OSError as e:
+         print("Error: %s : %s" % (SRC_IMG+'/'+GTYPE, e.strerror))
+         print("Error: %s : %s" % (DT_DIR+'/'+GTYPE, e.strerror))
