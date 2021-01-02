f3189ba8ade5e5ab3fc520d9285b49c2bb2ab28b
mriantf9 <muhamad@riantf.id>
2021-01-02 20:36:02 +0700

    bismillah

--- realtime_script/rtrunning_mail.sh
+++ realtime_script/rtrunning_mail.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+my_touch() {
+ if test -f $1
+ then
+  rm -f $1
+  touch $1
+ else
+  touch $1
+ fi
+}
 
+user=`whoami`
+DIR="/home/${user}/script_skripsi"
+logrt="${DIR}/REALTIME/log"
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+dt=`date '+%Y%m%d'`
 echo "Process sent email to ${1} ...."
 
-/usr/local/bin/python3.8 ${WORKDIR}/rtsent_mail.py $1 $2 $3
+my_touch ${logrt}/${dt}_$3_$2.log
+/usr/local/bin/python3.8 ${WORKDIR}/rtsent_mail.py $1 $2 $3 >> ${logrt}/${dt}_$3_$2.log
